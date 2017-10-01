e23d7df0567a7ffff1cf8a2abc93e1774358a124
kavaliabhilash <kavaliabhilash087@gmail.com>
2017-10-01 16:45:30 +0530

    comitted after bootstrap and css changes

--- online-example/src/main/java/com/eample/online/onlineexample/controller/Pagecontroller.java
+++ online-example/src/main/java/com/eample/online/onlineexample/controller/Pagecontroller.java
@@ -1,21 +1,20 @@
 package com.eample.online.onlineexample.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Pagecontroller {
 
-		@RequestMapping(value = {"/","/home","/index"})
+		@RequestMapping(value = {"/*","/home","/index"})
 		public ModelAndView index(){
 			ModelAndView mv = new ModelAndView("page");
 			mv.addObject("greeting","wlecome to");
 			return mv;
 		}
 		
-		@RequestMapping(value="/test/{greeting}")
+		/*@RequestMapping(value="/test/{greeting}")
 		public ModelAndView test(@PathVariable("greeting")String greeting){
 			if (greeting == null){
 				greeting = "Helloo";
@@ -24,6 +23,6 @@ public class Pagecontroller {
 			ModelAndView mv = new ModelAndView("page");
 			mv.addObject("greeting",greeting);
 			return mv;	
-		}
+		}*/
 		}
 
